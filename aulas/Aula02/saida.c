0cb5b96a8c2745f234020e5522db8c88d26c8aa2
unknown <2612082031@ACADEMICO.IESB.BR>
2026-03-11 10:58:00 -0300

    adciona codigos da aula02

---
+++ aulas/Aula02/saida.c
@@ -0,0 +1,21 @@
+/*
+Este codigo explica como uar a funçao Prinf
+
+
+*/
+
+#include <stdio.h> //inclusao de bibliotecas da linguagem
+
+int main() {
+//1.Entrada
+
+//2.processamento
+
+//3.saida
+printf(" um texto qualquer\n");
+printf("Programar em C eh Top\n");
+printf("%i\n", 10);
+printf("%f\n",10.3455);
+
+ return 0;
+}
