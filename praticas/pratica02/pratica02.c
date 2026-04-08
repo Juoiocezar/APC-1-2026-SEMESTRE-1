20fdd97cb04b6ffb15bc89af26d7a239076a7da3
Juoiocezar <Juoiocezar>
2026-04-08 15:49:17 -0300

    feat: corrigi pratica02. Fecha #2

--- praticas/pratica02/pratica02.c
+++ praticas/pratica02/pratica02.c
@@ -19,15 +19,31 @@ printf("%i\n", n2);
 printf("%.5i\n", n3);
 printf("%e\n", pi);
 
+int q1;  // quantidade 1
+q1 = 2;
+int q2;  // quantidade 2
+q2 = 1;
+int q3;  // quantidade 3
+q3 = 3;
+float v1;  // valor do produto 1
+v1 = 39.99;
+float v2;   // valor do produto 2
+v2 = 89.90;
+float v3;   // valor do produto 3
+v3 = 19.99;                                               
+float vt;  // valor total
+vt = 229.85;
+
+
 printf("==============================\n");
 printf("      N O T A    L E G A L\n");
 printf("==============================\n");
 printf("Produto         Qtd Valor Unit\n");
-printf("Camiseta        002      39.99\n");
-printf("Calca           001      89.90\n");
-printf("Meia Social     003      19.99\n");
+printf("Camiseta        %i      %.2f\n", q1, v1);
+printf("Calca           %i      %.2f\n", q2, v2);
+printf("Meia Social     %i      %.2f\n", q3, v3);
 printf("==============================\n");
-printf("Total                   229.85\n");
+printf("Total                   %.2f\n", vt);
 
     return 0;
 }
