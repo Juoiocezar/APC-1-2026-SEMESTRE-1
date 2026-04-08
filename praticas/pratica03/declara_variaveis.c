70b33db9e78d543d50deec485728146c66e29024
Juoiocezar <Juoiocezar>
2026-04-08 20:02:37 -0300

    colocando pratica 03

---
+++ praticas/pratica03/declara_variaveis.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+int id; // idade
+id = 18;
+char sx; // sexo
+sx = 'M';
+float alt; // altura
+alt = 1.85;
+float ps; // peso
+ps = 60;
+
+printf("sua idade eh:%i\n", id);
+printf("seu sexo eh:%c\n", 'M');
+printf("sua altura eh:%.2f\n", alt);
+printf("seu peso eh:%.2f\n", ps);
+
+    return 0;
+}
